89f26a07c9f43e880d79dd11ca40a85e2ed48bbd
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-24 22:42:32 -0700

    Replace GCC builtins with C23 atomics

--- modern/tests/spinlock_test.sh
+++ modern/tests/spinlock_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cc -pthread spinlock_test.c ../../v10/ipc/spinlock.c -I ../../v10/ipc/h -DSMP_ENABLED -o spinlock_test
+cc -std=c23 -pthread spinlock_test.c ../../v10/ipc/spinlock.c -I ../../v10/ipc/h -DSMP_ENABLED -o spinlock_test
 ./spinlock_test > spinlock_output.txt
 if grep -q "counter=200000" spinlock_output.txt; then
     echo "spinlock test passed"
